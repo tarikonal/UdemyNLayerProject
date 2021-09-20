f659e17cda3bebc4b1d6f5fa16a75a3f1c74578c
TARIK ONAL <tarikonal@gmail.com>
2021-09-20 10:52:20 +0300

    Service katmanı tamamlandı

--- UdemyNLayerProject.Service/Services/CategoryService.cs
+++ UdemyNLayerProject.Service/Services/CategoryService.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using UdemyNLayerProject.Core.Models;
@@ -11,16 +10,15 @@ using UdemyNLayerProject.Core.UnitOfWorks;
 
 namespace UdemyNLayerProject.Service.Services
 {
-    public class ProductService : Service<Product>, IProductService
+    public class CategoryService : Service<Category>, ICategoryService
     {
-        public ProductService(IUnitOfWork unitOfWork, IRepository<Product> repository) : base(unitOfWork, repository)
+        public CategoryService(IUnitOfWork unitOfWork, IRepository<Category> repository) : base(unitOfWork, repository)
         {
         }
 
-        public async Task<Product> GetWithCategoryByIdAsync(int productId)
+        public async Task<Category> GetWithProductsByIdAsync(int categoryId)
         {
-          return await _unitOfWork.Products.GetWithCategoryByIdAsync(productId);
-
+           return await _unitOfWork.Categories.GetWithProductsByIdAsync(categoryId);
         }
     }
 }
