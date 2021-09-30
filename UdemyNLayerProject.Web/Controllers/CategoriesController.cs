d5a5378c797fcd46872e2473811e8c987f50297f
TARIK ONAL <tonal@jandarma.gov.tr>
2021-09-30 09:15:13 +0300

    yeni web katmanı eklendi

---
+++ UdemyNLayerProject.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+using UdemyNLayerProject.Core.Services;
+using UdemyNLayerProject.Web.DTOs;
+
+namespace UdemyNLayerProject.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
+        {
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            await _categoryService.AddAsync(_mapper.Map<Category>(categoryDto));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(CategoryDto categoryDto)
+        {
+            _categoryService.Update(_mapper.Map<Category>(categoryDto));
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(CategoryDto categoryDto)
+        {
+             _categoryService.Remove(_mapper.Map<Category>(categoryDto));
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ UdemyNLayerProject.Web/Mapping/MapProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyNLayerProject.Core.Models;
+using UdemyNLayerProject.Web.DTOs;
+
+namespace UdemyNLayerProject.Web.Mapping
+{
+    public class MapProfile:Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryWithProductDto>().ReverseMap();
+            CreateMap<Product, ProductWithCategoryDto>().ReverseMap();
+        }
+    }
+}
